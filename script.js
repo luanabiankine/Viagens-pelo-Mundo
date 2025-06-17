50ae986b3ad1d2a40dea6e5c6a7c20277b00d22b
luanabiankine <Luanabiankinecacho@gmail.com>
2025-06-17 14:07:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+function enviarFormulario() {
+  alert("Obrigado pelo contato! Retornaremos em breve.");
+}
+function enviarFormulario() {
+  // Aqui você pode adicionar lógica para salvar ou enviar os dados, se necessário
+  window.location.href = "contato-confirmado.html";
+}
